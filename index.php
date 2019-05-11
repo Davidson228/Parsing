0bb2d2a222f8c5bd0ab8291a15e3ccd656558add
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-05-11 09:53:38 +0300

    test

--- index.php
+++ index.php
@@ -8,3 +8,10 @@ $app->initLayout('Centered');
 $html = file_get_contents('https://www.elkor.lv/rus/elektronika-1/speles/datorspeles/?game_age_restriction=5305');
 
 phpQuery::newDocument($html);
+
+
+
+
+
+
+phpQuery::unloadDocuments($html);
