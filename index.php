dd04fff49e12ce6ac749d9849aaaf2bf81997105
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-05-11 10:06:30 +0300

    test

--- index.php
+++ index.php
@@ -9,7 +9,10 @@ $html = file_get_contents('https://www.elkor.lv/rus/elektronika-1/speles/datorsp
 
 phpQuery::newDocument($html);
 
+$title = pq('#products-grid-table')->text();
+//$title = explode('Компьютерная игра',$title);
 
+var_dumb($title);
 
 
 
