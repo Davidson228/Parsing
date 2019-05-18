f6fdc7650a32cf843b3efb9a34df9375a3d57600
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-05-18 10:33:23 +0300

    test

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 require 'vendor/autoload.php';
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=heroku_deabe0130a943ba;host=eu-cdbr-west-02.cleardb.net','b118774f4919d0:','6f6bc49f');
+\atk4\data\Persistence_SQL('mysql:dbname=heroku_deabe0130a943ba;host=eu-cdbr-west-02.cleardb.net','b118774f4919d0','6f6bc49f');
 
 class Da extends \atk4\data\Model {
  public $table = 'da';
